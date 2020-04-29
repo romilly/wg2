5a579083e2e0e31cda1db86b2a34dd53d2d1b292
Romilly Cocking <romilly.cocking@gmail.com>
2020-04-29 15:05:04 +0100

    Writing (unconverted) files.

--- src/wg2/builder.py
+++ src/wg2/builder.py
@@ -1,4 +1,68 @@
+import os
+import re
+
+
+def read(f):
+    with open(f) as input_file:
+        return input_file.read()
+
+
+class MarkdownPage:
+    def __init__(self, root, filename):
+        self.root = root
+        self.filename = filename
+
+    def contents(self):
+        return read(self.path())
+
+    def path(self):
+        return os.path.join(self.root, self.filename)
+
+
+class HtmlPage:
+    def __init__(self, directory, filename, contents):
+        self._contents = contents
+        self.filename = filename
+        self.directory = directory
+
+    def path(self):
+        return os.path.join(self.directory, self.filename)
+
+    def contents(self):
+        return self._contents
+
+
+class PageWriter:
+    def write(self, html_page: HtmlPage):
+        os.makedirs(html_page.directory, exist_ok=True)
+        with open(html_page.path(),'w') as html_file:
+            html_file.write(html_page.contents())
+
+
+class MarkdownConverter:
+    HTML_RE = re.compile("\.md$")
+
+    def __init__(self, target_directory, page_writer: PageWriter, content_directory):
+        self.content_directory = content_directory
+        self.page_writer = page_writer
+        self.target_directory = target_directory
+
+    def convert(self, markdown_page: MarkdownPage):
+        html_filename = self.HTML_RE.sub('.html', markdown_page.filename)
+        relative_path = os.path.relpath(markdown_page.root, self.content_directory )
+        html_path = os.path.join(self.target_directory, relative_path)
+        html_page = HtmlPage(html_path, html_filename, markdown_page.contents())
+        self.page_writer.write(html_page)
+
+
 
 class SiteBuilder:
     def build(self, content_directory, target_directory):
-        pass
+        page_writer=PageWriter()
+        converter = MarkdownConverter(target_directory, page_writer, content_directory)
+        for root, directories, files in os.walk(content_directory):
+            for f in files:
+                if f.endswith('.md'):
+                    markdown_page = MarkdownPage(root, f)
+                    converter.convert(markdown_page)
+                    
--- src/wg2/test/acceptance/e2e.py
+++ src/wg2/test/acceptance/e2e.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import unittest
 
 from hamcrest import assert_that
@@ -6,12 +8,19 @@ from hamcrest_helpers.files import is_empty_directory, contains_files
 from wg2.builder import SiteBuilder
 
 
+def empty_directory(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    os.mkdir(directory)
+
+
 class EndToEndTestCase(unittest.TestCase):
+    def setUp(self):
+        self.site_builder = SiteBuilder()
+        empty_directory('generated')
+
     def test_html_pages_are_generated_from_markdown(self):
         assert_that('generated', is_empty_directory())
-        SiteBuilder().build('content','generated')
+        self.site_builder.build('content','generated')
         assert_that('generated', contains_files('index.html','about/about.html', 'contact/contact.html'))
 
-
-if __name__ == '__main__':
-    unittest.main()
